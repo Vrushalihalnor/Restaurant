9ef4c30b52d6ad7fa1f71763886ba5dd27cae4b1
vrushali halnor <vrushalihalnor40@gmail.com>
2025-11-27 03:42:00 -0800

    your message

--- MyApi/obj/Debug/net10.0/MyApi.AssemblyInfo.cs
+++ MyApi/obj/Debug/net10.0/MyApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1917f975142f1fd292072278c48acc7f76342921")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
